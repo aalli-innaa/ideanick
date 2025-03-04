5448dc8ed48e9b438960fb12ae1034e19038d3fd
aalli-innaa <a3372442@gmail.com>
2025-03-04 09:32:45 +0600

    Дошла до 35 видео

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express from "express";
+import { applyTrpcToExpressApp } from "./lib/trpc";
+import { trpcRouter } from "./router";
+
+const expressApp = express();
+expressApp.use(cors());
+
+expressApp.get("/ping", (req, res) => {
+  res.send("pong");
+});
+applyTrpcToExpressApp(expressApp, trpcRouter);
+expressApp.listen(3000, () => {
+  console.info(` Server listening on http://localhost:3000`);
+});
